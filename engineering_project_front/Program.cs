cbb29e09f08f9635b54d75bce97baefa5d9c7cb8
Wojciech Olech <wojtek.olech@o2.pl>
2024-10-30 13:26:14 +0100

    Added Syncfusion

--- engineering_project_front/Program.cs
+++ engineering_project_front/Program.cs
@@ -1,15 +1,24 @@
 using engineering_project_front;
 using engineering_project_front.Services;
 using engineering_project_front.Services.Interfaces;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.Popups;
 
 namespace Program
 {
     public class Program
     {
+        //Temporary, I don't like it here but I don't know where to put this for now
+        private const string SYNCFUSION_KEY = "Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXhfcXRdRGRfUUR0Wks=";
+
         public static async Task Main(string[] args)
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(SYNCFUSION_KEY);
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -17,6 +26,10 @@ namespace Program
             builder.Services.AddHttpClient("engineering-project", options => options.BaseAddress = new Uri("https://localhost:7059/"));
 
             builder.Services.AddScoped<ITestService, TestService>();
+
+            builder.Services.AddScoped<SfDialogService>();
+
+            builder.Services.AddSyncfusionBlazor();
 
             await builder.Build().RunAsync();
         }
