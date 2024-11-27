7ad0fb733d52543b16886fa96a2a843252894064
Wojciech Olech <wojtek.olech@o2.pl>
2024-11-27 12:52:25 +0100

    Added token Checker

--- engineering_project_front/Layout/MainLayout.razor.cs
+++ engineering_project_front/Layout/MainLayout.razor.cs
@@ -1,6 +1,26 @@
-﻿namespace engineering_project_front.Layout
+﻿
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+
+namespace engineering_project_front.Layout
 {
     public partial class MainLayout
     {
+        [Inject]
+        private ISessionStorageService sessionStorage { get; set; } = default!;
+        [Inject]
+        private NavigationManager navManager { get; set; } = default!;
+        protected async override Task OnInitializedAsync()
+        {
+            await CheckForToken();
+
+            await base.OnInitializedAsync();
+        }
+
+        private async Task CheckForToken()
+        {
+            if (!await sessionStorage.ContainKeyAsync("token"))
+                navManager.NavigateTo("/");
+        }
     }
 }
